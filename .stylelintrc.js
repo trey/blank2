eb0c86bce9fdd19848999b57fe0eb14a9b2ab051
Trey Piepmeier <trey@treypiepmeier.com>
2019-09-29 11:36:16 -0400

    Add reset styles and update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": "stylelint-config-standard",
     "rules": {
         "indentation": 4,
-        "string-quotes": "single"
-    }
+        "string-quotes": "single",
+        "at-rule-no-unknown": null,
+    },
+    "ignoreFiles": "src/_includes/scss/partials/vendor/**",
 }
