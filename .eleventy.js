15631ff06d44e66396f8efe6c1a485b02b12a612
Trey Piepmeier <trey@treypiepmeier.com>
2019-10-04 16:17:35 -0400

    Copy images images to the dist folder
    A small bit of functionality from https://github.com/trey/avarmint-11ty

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ const md = new MarkdownIt();
 module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter('markdown', value => md.renderInline(value));
 
+    // Copy unaltered original images.
+    eleventyConfig.addPassthroughCopy('src/img');
+
     return {
         dir: {
             input: 'src',
