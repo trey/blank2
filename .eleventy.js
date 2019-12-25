229713c9c53be9ee3008d9dc864a0ebc36147c94
Trey Piepmeier <trey@treypiepmeier.com>
2019-12-25 16:46:25 -0500

    Add 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const MarkdownIt = require('markdown-it');
 const md = new MarkdownIt();
 
@@ -6,6 +7,21 @@ module.exports = function(eleventyConfig) {
 
     // Copy unaltered original images.
     eleventyConfig.addPassthroughCopy('src/img');
+
+    // Make 404 page work with `eleventy --serve`
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function(err, browserSync) {
+                const content_404 = fs.readFileSync('dist/404.html');
+
+                browserSync.addMiddleware('*', (req, res) => {
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
 
     return {
         dir: {
